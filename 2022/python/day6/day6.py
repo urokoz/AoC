ce9f091ed3838a7e2f5ff7cfa200d3f11bae6ed0
urokoz <urokoz@gmail.com>
2022-12-06 13:06:05 +0100

    2022/day6 done

---
+++ 2022/python/day6/day6.py
@@ -0,0 +1,26 @@
+
+def part1():
+    with open("input.txt", "r") as infile:
+        signal = infile.readline()
+
+    for i in range(len(signal)-4):
+        temp = signal[i:i+4]
+        
+        if len(set(temp)) == 4:
+            return i+4
+
+
+def part2():
+    with open("input.txt", "r") as infile:
+        signal = infile.readline()
+
+    for i in range(len(signal)-14):
+        temp = signal[i:i+14]
+
+        if len(set(temp)) == 14:
+            return i+14
+
+
+if __name__ == "__main__":
+    print(part1())
+    print(part2())
